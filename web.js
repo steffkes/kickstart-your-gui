138be3d7bc31d8aac8300c1b7db87f92525b06d6
steffkes <matheis.stefan@googlemail.com>
2012-05-11 19:50:51 +0200

    app.use for express.logger

--- web.js
+++ web.js
@@ -1,6 +1,8 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express.createServer();
+
+app.use(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World!\n');
