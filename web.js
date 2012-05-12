fcf51e22651836c8b93f163c2d37eaebf55ca186
steffkes <matheis.stefan@googlemail.com>
2012-05-12 20:22:20 +0200

    use RedisStore for express.session

--- web.js
+++ web.js
@@ -1,36 +1,21 @@
 var express = require('express');
 var url = require('url');
 var oauth = require('oauth').OAuth;
+var redis_lib = require('redis');
+var RedisStore = require('connect-redis')(express);
 
-// -- expressjs
+// -- redis
 
-var app = express.createServer();
-app.use( express.logger() );
-app.use( express.bodyParser() );
-app.use( express.cookieParser() );
-app.use( express.session( { key: 'session_id', secret: "MC44NzA2OTIwMCAxMzM2ODM5MDY1IzRmYWU4Yjk5ZDQ5NzM" } ) );
+var redis_url  = url.parse( process.env.REDISTOGO_URL || 'redis://0:secret@localhost:9104/' );
+var redis_data = redis_url.auth.split( ':' );
+var redis_db = redis_data[0];
+var redis_pass = redis_data[1];
 
-app.configure
-(
-  'development',
-  function() {
-
-    process.env.REDISTOGO_URL = 'redis://0:secret@localhost:9104/'
-    process.env.PORT = 5000;
-
-  }
-);
-
-// -- redistogo
-
-var parsed_url  = url.parse( process.env.REDISTOGO_URL );
-var parsed_auth = parsed_url.auth.split( ':' );
-
-var redis = require('redis').createClient( parsed_url.port, parsed_url.hostname );
+var redis = redis_lib.createClient( redis_url.port, redis_url.hostname );
 
 redis.auth
 (
-  parsed_auth[1],
+  redis_pass,
   function(err) {
 
     if (err) throw err;
@@ -38,14 +23,14 @@ redis.auth
   }
 );
 
-redis.select( parsed_auth[0] );
+redis.select( redis_db );
 redis.on
 (
   'connect',
   function() {
 
     redis.send_anyways = true
-    redis.select( parsed_auth[0] );
+    redis.select( redis_db );
     redis.send_anyways = false;
 
   }
@@ -57,6 +42,24 @@ redis.on
   function (err) {
 
     console.log( 'redis error (' + redis.host + ':' + redis.port + '): ' + err );
+
+  }
+);
+
+// -- expressjs
+
+var app = express.createServer();
+app.use( express.logger() );
+app.use( express.bodyParser() );
+app.use( express.cookieParser() );
+app.use( express.session( { key: 'session_id', secret: "MC44NzA2OTIwMCAxMzM2ODM5MDY1IzRmYWU4Yjk5ZDQ5NzM", store: new RedisStore( { client : redis } ) } ) );
+
+app.configure
+(
+  'development',
+  function() {
+
+    process.env.PORT = 5000;
 
   }
 );
